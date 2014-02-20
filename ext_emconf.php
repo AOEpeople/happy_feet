3206ce9d0726bcfb8cab62e19ac07a8473103d21
tifx <tifx@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-02-20 15:28:39 +0000

    added emconf

---
+++ ext_emconf.php
@@ -0,0 +1,31 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title'                         => 'Footnotes',
+	'description'                   => 'Inserts footnotes',
+	'category'                      => 'plugin',
+	'author'                        => 'AOE',
+	'author_email'                  => 'dev@aoe.com',
+	'shy'                           => '',
+	'dependencies'                  => 'extbase',
+	'conflicts'                     => '',
+	'priority'                      => '',
+	'module'                        => '',
+	'state'                         => 'alpha',
+	'internal'                      => '',
+	'uploadfolder'                  => 0,
+	'createDirs'                    => '',
+	'modify_tables'                 => '',
+	'clearCacheOnLoad'              => 0,
+	'lockType'                      => '',
+	'author_company'                => '',
+	'version'                       => '1.0.dev',
+
+	'constraints'                   => array(
+		'depends'   => array(
+		),
+		'conflicts' => array(),
+		'suggests'  => array(),
+	),
+	'_md5_values_when_last_written' => '',
+);
