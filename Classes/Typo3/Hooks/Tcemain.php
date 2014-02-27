94e6100ea815c5ffad7d7ad36d39e7fe0be24128
tifx <tifx@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-02-27 09:39:08 +0000

    changed tcs for options enrichments ans changed eftFootnotes viewHelper

--- Classes/Typo3/Hooks/Tcemain.php
+++ Classes/Typo3/Hooks/Tcemain.php
@@ -1,4 +1,27 @@
 <?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2014 AOE GmbH <dev@aoe.com>
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 /**
  * Class which provides TCE main hooks.
@@ -21,7 +44,7 @@ class Tx_HappyFeet_Typo3_Hooks_Tcemain
     public function processDatamap_postProcessFieldArray($status, $table, $id, &$fieldArray, $tcemain)
     {
         if ($status === 'new' && $table == 'tx_happyfeet_domain_model_footnote') {
-            $fieldArray['index_number'] = $this->getFootnoteRepository()->getLowestFreeIndex();
+            $fieldArray['index_number'] = $this->getFootnoteRepository()->getLowestFreeIndexNumber();
         }
     }
 
