3c358be2fdbab7b82b059ab08ace049538f15b96
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2017-11-15 14:11:49 +0100

    Merged TYPO3V7 into master

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,11 +19,10 @@ $EM_CONF[$_EXTKEY] = array(
     'clearCacheOnLoad' => 0,
     'lockType' => '',
     'author_company' => 'AOE GmbH',
-    'version' => '1.9.1',
+    'version' => '3.0.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-0.0.0',
-            'php' => '5.3.0-5.5.99',
+            'typo3' => '7.6.0-0.0.0',
             'linkhandler' => ''
         ),
         'conflicts' => array(),
