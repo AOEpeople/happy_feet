7e6926fe67e14f0db308fb69f96cb144f37af71b
Christian Krzeminski <christian.krzeminski@aoe.com>
2019-04-05 08:35:51 +0200

    [TASK] TYPO3V8 compatible

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '4.0.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.6.0-7.6.99',
+            'typo3' => '7.6.0-8.7.99',
             'linkhandler' => ''
         ),
         'conflicts' => array(),
