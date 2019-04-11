a345bed9838959e4fc7813f2958af6205d4d2f85
Christian Krzeminski <christian.krzeminski@aoe.com>
2019-04-11 13:33:54 +0200

    Revert "[Fix] use nativ html renderer"
    This reverts commit d4f3090b

--- Classes/ViewHelpers/FlatifyViewHelper.php
+++ Classes/ViewHelpers/FlatifyViewHelper.php
@@ -25,7 +25,8 @@ namespace AOE\HappyFeet\ViewHelpers;
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
-use \TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+
 /**
  * @package HappyFeet
  * @subpackage Service_Test
