fee857b0796631a730762d046cdc3ffa032e63f3
Felix Semmler <felix.semmler@aoe.com>
2022-02-14 11:36:04 +0100

    [FEATURE] TYPO3 v11 refactoring

--- Configuration/TCA/tx_happyfeet_domain_model_footnote.php
+++ Configuration/TCA/tx_happyfeet_domain_model_footnote.php
@@ -5,7 +5,7 @@ if (!defined('TYPO3_MODE')) {
 
 return [
     'ctrl' => [
-        'title' => 'LLL:EXT:happy_feet/Resources/Private/Language/locallang_db.xml:tx_happyfeet_domain_model_footnote',
+        'title' => 'LLL:EXT:happy_feet/Resources/Private/Language/locallang_db.xlf:tx_happyfeet_domain_model_footnote',
         'label' => 'title',
         'tstamp' => 'tstamp',
         'crdate' => 'crdate',
@@ -19,37 +19,29 @@ return [
     'interface' => [
         'showRecordFieldList' => 'title,index_number'
     ],
-    'columns'   => [
-        't3ver_label'      => [
-            'label'  => 'LLL:EXT:lang/Resources/Private/Language/locallang_general.xml:LGL.versionLabel',
-            'config' => [
-                'type' => 'input',
-                'size' => '30',
-                'max'  => '30',
-            ]
-        ],
+    'columns' => [
         'sys_language_uid' => [
             'exclude' => 1,
-            'label'   => 'LLL:EXT:lang/Resources/Private/Language/locallang_general.xml:LGL.language',
+            'label'   => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.language',
             'config'  => [
                 'type'                => 'select',
                 'renderType'          => 'selectSingle',
                 'foreign_table'       => 'sys_language',
                 'foreign_table_where' => 'ORDER BY sys_language.title',
                 'items'               => [
-                    ['LLL:EXT:lang/Resources/Private/Language/locallang_general.xml:LGL.allLanguages', -1],
-                    ['LLL:EXT:lang/Resources/Private/Language/locallang_general.xml:LGL.default_value', 0]
+                    ['LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.allLanguages', -1],
+                    ['LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.default_value', 0]
                 ]
             ]
         ],
-        'l18n_parent'      => [
+        'l18n_parent' => [
             'displayCond' => 'FIELD:sys_language_uid:>:0',
             'exclude'     => 1,
             'label'       => 'LLL:EXT:lang/Resources/Private/Language/locallang_general.xml:LGL.l18n_parent',
             'config'      => [
-                'type'                => 'select',
-                'renderType'          => 'selectSingle',
-                'items'               => [
+                'type'        => 'select',
+                'renderType'  => 'selectSingle',
+                'items'       => [
                     ['', 0],
                 ],
                 'foreign_table'       => 'tx_happyfeet_domain_model_footnote',
@@ -62,42 +54,42 @@ return [
                 'default' => '0'
             ]
         ],
-        'hidden'           => [
+        'hidden' => [
             'exclude' => 1,
-            'label'   => 'LLL:EXT:lang/Resources/Private/Language/locallang_general.xml:LGL.hidden',
+            'label'   => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.hidden',
             'config'  => [
                 'type'    => 'check',
                 'default' => '0'
             ]
         ],
-        'starttime'        => [
+        'starttime' => [
             'exclude' => 1,
-            'label'   => 'LLL:EXT:lang/Resources/Private/Language/locallang_general.xml:LGL.starttime',
+            'label'   => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.starttime',
             'config'  => [
-                'type'     => 'input',
+                'type'       => 'input',
                 'renderType' => 'inputDateTime',
-                'size'     => '8',
-                'eval'     => 'date',
-                'default'  => '0',
-                'checkbox' => '0'
+                'size'       => '8',
+                'eval'       => 'date',
+                'default'    => '0',
+                'checkbox'   => '0'
             ]
         ],
-        'endtime'          => [
+        'endtime' => [
             'exclude' => 1,
-            'label'   => 'LLL:EXT:lang/Resources/Private/Language/locallang_general.xml:LGL.endtime',
+            'label'   => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.endtime',
             'config'  => [
-                'type'     => 'input',
+                'type'       => 'input',
                 'renderType' => 'inputDateTime',
-                'size'     => '8',
-                'eval'     => 'date',
-                'checkbox' => '0',
-                'default'  => '0',
-                'range'    => 'must be configured in ext_tables.php, behause the configuration-data is not cacheable!'
+                'size'       => '8',
+                'eval'       => 'date',
+                'checkbox'   => '0',
+                'default'    => '0',
+                'range'      => 'must be configured in ext_tables.php, behause the configuration-data is not cacheable!'
             ]
         ],
         'index_number' => [
             'exclude' => 1,
-            'label'   => 'LLL:EXT:happy_feet/Resources/Private/Language/locallang_db.xml:tx_happyfeet_domain_model_footnote.index_number',
+            'label'   => 'LLL:EXT:happy_feet/Resources/Private/Language/locallang_db.xlf:tx_happyfeet_domain_model_footnote.index_number',
             'config'  => [
                 'type' => 'none',
                 'size' => 30,
@@ -105,7 +97,7 @@ return [
         ],
         'title' => [
             'exclude' => 0,
-            'label'   => 'LLL:EXT:happy_feet/Resources/Private/Language/locallang_db.xml:tx_happyfeet_domain_model_footnote.title',
+            'label'   => 'LLL:EXT:happy_feet/Resources/Private/Language/locallang_db.xlf:tx_happyfeet_domain_model_footnote.title',
             'config'  => [
                 'type' => 'input',
                 'size' => 30,
@@ -114,7 +106,7 @@ return [
         ],
         'header' => [
             'exclude' => 0,
-            'label'   => 'LLL:EXT:happy_feet/Resources/Private/Language/locallang_db.xml:tx_happyfeet_domain_model_footnote.header',
+            'label'   => 'LLL:EXT:happy_feet/Resources/Private/Language/locallang_db.xlf:tx_happyfeet_domain_model_footnote.header',
             'config'  => [
                 'type' => 'input',
                 'size' => 30,
@@ -123,7 +115,7 @@ return [
         ],
         'description' => [
             'exclude' => 0,
-            'label'   => 'LLL:EXT:happy_feet/Resources/Private/Language/locallang_db.xml:tx_happyfeet_domain_model_footnote.description',
+            'label'   => 'LLL:EXT:happy_feet/Resources/Private/Language/locallang_db.xlf:tx_happyfeet_domain_model_footnote.description',
             'config'  => [
                 'type' => 'text',
                 'cols' => '30',
@@ -133,10 +125,10 @@ return [
             ],
         ]
     ],
-    'types'     => [
-        '0' => ['showitem' => 'sys_language_uid, l18n_parent, l18n_diffsource, title, index_number, header, description, --div--;LLL:EXT:happy_feet/Resources/Private/Language/locallang_db.xml:tx_happyfeet_domain_model_footnote.tab.access, hidden,--palette--;;1']
+    'types' => [
+        '0' => ['showitem' => 'title, index_number, header, description, --div--;LLL:EXT:happy_feet/Resources/Private/Language/locallang_db.xlf:tx_happyfeet_domain_model_footnote.tab.access, hidden,--palette--;;1']
     ],
-    'palettes'  => [
+    'palettes' => [
         '1' => ['showitem' => 'starttime, endtime']
     ]
 ];
