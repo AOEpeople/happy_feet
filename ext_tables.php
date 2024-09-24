78fb3c9c263722ef969d875ddf63fe79e459c66f
Christian Krzeminski <christian.krzeminski@aoe.com>
2024-09-24 13:36:00 +0200

    fix: broken range config

--- ext_tables.php
+++ ext_tables.php
@@ -1,10 +1,4 @@
 <?php
 defined('TYPO3') or die();
 
-// configure range of endtime here (and NOT in Configuration/TCA/[tablename].php), because the configuration-data is not cacheable!
-$GLOBALS['TCA']['tx_happyfeet_domain_model_footnote']['columns']['endtime']['config']['range'] = [
-    'upper' => mktime(0, 0, 0, 12, 31, date('Y') + 10),
-    'lower' => mktime(0, 0, 0, date('m') - 1, date('d'), date('Y'))
-];
-
 $GLOBALS['TCA']['tx_happyfeet_domain_model_footnote']['ctrl']['security']['ignorePageTypeRestriction'] = true;
